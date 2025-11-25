7959631ae869aef9fa786990c7d8e193445868c7
PhoenixCrew2025 <support@easycreate.org>
2025-11-25 15:58:28 -0500

    Fixed the Custom Enchantments

---
+++ src/main/java/net/phoenixcrew2025/FreeStyleV2/datagen/ModDatapackProvider.java
@@ -0,0 +1,21 @@
+package net.phoenixcrew2025.FreeStyleV2.datagen;
+
+import net.phoenixcrew2025.FreeStyleV2.FreeStyleV2;
+import net.phoenixcrew2025.FreeStyleV2.enchantment.ModEnchantments;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.ENCHANTMENT, ModEnchantments::bootstrap);
+
+    public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(FreeStyleV2.MOD_ID));
+    }
+}
